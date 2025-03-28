e1fc4cbfa91d526bdc7ca00542a239c5a1623da9
JustCodeandChill <paul.tran189@gmail.com>
2025-03-27 23:22:58 -0500

    add listing product and calling api scaffold

--- sports-center-client/src/App.tsx
+++ sports-center-client/src/App.tsx
@@ -1,33 +1,45 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+      const fetchData = async () => {
+          try {
+            const response = await fetch("http://localhost:8081/api/products/all");
+            if (!response.ok) {
+                throw new Error("Fail to fetch data");
+            }
+            const data = await response.json();
+            setProducts(data.content);
+
+          }catch (e) {
+              console.log("error fetching data" + e);
+          }
+      };
+      fetchData();
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div>
+            <h1>Sport center</h1>
+            <ul>
+                {
+                    products.map(product => {
+                        return <li key={product.id}>
+                            <p>Name: {product.name}</p>
+                            <p>Price {product.price}</p>
+                            <p>Description: {product.description}</p>
+                            <p>Brand: {product.brandName}</p>
+                            <p>Type: {product.typeName}</p>
+                        </li>
+                    })
+                }
+            </ul>
+        </div>
     </>
   )
 }

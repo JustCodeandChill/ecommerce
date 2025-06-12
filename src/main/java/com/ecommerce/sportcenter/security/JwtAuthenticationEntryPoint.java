a4cc499545f0fbd1612355cf3cca08d69803ee8a
JustCodeandChill <paul.tran189@gmail.com>
2025-06-12 12:17:09 -0500

    add JWT, JWT token gen and parser

---
+++ src/main/java/com/ecommerce/sportcenter/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,18 @@
+package com.ecommerce.sportcenter.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException {
+        httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        PrintWriter writer = httpServletResponse.getWriter();
+        writer.println("Access Denied " + e.getMessage());
+    }
+}
